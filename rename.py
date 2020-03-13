eea1e3dc6f4634ee3d291024473787898162d067
Ben Hodges <benjaminhodges07@gmail.com>
2020-03-13 01:31:10 +0000

    Initial commit

---
+++ rename.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import hashlib
+
+#from stack overflow
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+if len(sys.argv) != 2:
+    print("Please enter path to rename:")
+    exit()
+
+for (dirpath, dirnames, filenames) in os.walk(sys.argv[1]):
+    #print(dirpath, dirnames, filenames)
+    for name in filenames:
+        if name[0] == "." or '.' not in name:
+            continue
+        path = os.path.join(dirpath,name)
+        ext = name.split(".")[-1]
+        if ext == "jfif":
+            ext = "jpg"
+        if ext not in {"jpg","jpeg","png"}:
+            continue
+        newname = md5(path) + "." + ext
+        if newname != name:
+            try:
+                newpath =  os.path.join(dirpath,newname)
+                os.rename(path,newpath)
+            except:
+                print("File exists:",newname)
+                os.remove(path)
+            
+print("Done")
